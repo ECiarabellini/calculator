eb4df467f494ffdc574ba7b65b15c6b7262daad1
ECiarabellini <emilyciarabellini@gmail.com>
2023-07-17 15:50:55 -0700

    Add non-functioning HTML calculator

--- main.js
+++ main.js
@@ -12,5 +12,14 @@ function multiply(a, b) {
 
 function divide(a,b) {
     return a/b;
+};
+
+let var1;
+let operator; //options are add, subtract, multiply, divide. NOT strings.
+let var2;
+
+function operate (var1, operator, var2){
+    return operator(var1, var2);
 }
 
+console.log(operate(1, multiply, 2));
